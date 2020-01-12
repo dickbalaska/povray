8fca89a810bb917c85ccad8b5548e75893f49862
Dick Balaska <dick@buckosoft.com>
2020-01-11 23:00:24 -0500

    whitespace

--- source/parser/debugger.h
+++ source/parser/debugger.h
@@ -1,3 +1,23 @@
+/******************************************************************************
+ * debugger.h - The POV-Ray parser filter object
+ *
+ * qtpovray - A Qt IDE frontend for POV-Ray
+ * Copyright(c) 2020 - Dick Balaska, and BuckoSoft.
+ *
+ * qtpovray is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * qtpovray is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
 #ifndef DEBUGGER_H
 #define DEBUGGER_H
 
